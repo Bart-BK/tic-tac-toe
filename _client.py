8465615b596ee5a3e06cc9592a770d69a4cc3f31
Bart-BK <bartkoliveira@gmail.com>
2017-07-14 20:16:35 -0300

    Add client and server files

---
+++ _client.py
@@ -0,0 +1,101 @@
+from sys import argv, stdin, stdout
+import socket
+import select
+ 
+def chat_client():
+	if((len(argv) <= 2) or (len(argv)) > 4):
+		print ("Use : python _client.py hostname port language(BR|EN) ");
+		exit();	
+	elif(len(argv) == 3):
+		HOST = argv[1];
+		PORT = argv[2];
+		LANGUAGE = "EN";
+	elif(len(argv) == 4):
+		HOST = argv[1];
+		PORT = argv[2]; 
+		if(argv[3].lower() != "br" and argv[3].lower() != "en"):
+			LANGUAGE = input("Please, give a valid language:");
+			while(LANGUAGE.lower() != "br" and LANGUAGE.lower() != "en"):
+				LANGUAGE = input("Please, give a valid language (BR or EN):");
+		else:
+			LANGUAGE = argv[3];
+
+	LANGUAGE = LANGUAGE.lower();
+	if(LANGUAGE == "br"):
+		print("Interface esta definida para PT-BR");
+	else:
+		print("Interface is set to EN");
+
+	CONNECTION = (HOST, int(PORT));
+
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+	client_socket.settimeout(10);
+
+	while True:
+		try:
+			if(LANGUAGE == "br"):
+				print ("\nConectando a "+HOST+"::"+PORT);
+			else:
+				print ("\nConnecting to "+HOST+"::"+PORT);
+
+			client_socket.connect(CONNECTION);
+			break;
+		except:
+			if(LANGUAGE == "br"):
+				print ("Erro ao se conectar a "+HOST+"::"+PORT);
+				chose = input("[A]bortar, [M]odificar ou [T]entar novamente?");
+			else:
+				print ("Error in connection "+HOST+"::"+PORT);
+				chose = input("[A]bort, [C]hange ou [T]ry again?");
+
+			if(chose.lower() == 'a'):
+				exit();
+			elif((chose.lower() == 'm' and LANGUAGE == "br") or (chose.lower() == 'c' and LANGUAGE == "en")):
+				if(LANGUAGE == "br"):
+					HOST = input("Insira o HOST: ");
+					PORT = input("Insira a PORTA: ");
+				else:
+					HOST = input("Enter the HOST: ");
+					PORT = input("Enter the PORT: ");
+
+	if(LANGUAGE == "br"):
+		print ("Conexao estabelecida");
+		stdout.write('[Eu] '); stdout.flush();
+	else:
+		print ("Connection established");
+		stdout.write('[Me] '); stdout.flush();
+		
+	while 1:
+	    socket_list = [stdin, client_socket];
+	     
+	    # Get the list sockets which are readable
+	    ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
+	     
+	    for sock in ready_to_read:             
+	        if sock == client_socket:
+	            # incoming message from remote server, s
+	            data = sock.recv(4096)
+	            if not data :
+	                print ('\nDisconnected from chat server');
+	                exit()
+	            else :
+	                #print data
+	                stdout.write(data)
+	                if(LANGUAGE == "br"):
+	                	stdout.write('[Eu] '); stdout.flush();
+                	else:
+                		stdout.write('[Me] '); stdout.flush(); 
+	        
+	        else :
+	            # user entered a message
+				msg = stdin.readline();
+				client_socket.send(msg);
+				if(LANGUAGE == "br"):
+					stdout.write('[Eu] '); 
+				else:
+					stdout.write('[Me] ');
+				stdout.flush();
+
+if __name__ == "__main__":
+
+    exit(chat_client())
---
+++ _server.py
@@ -0,0 +1,103 @@
+import socket
+import select
+from sys import argv
+
+SOCKET_LIST = [];
+RECV_BUFFER = 4096;
+
+turn = 0;
+
+def chat_server():
+    if(len(argv) < 2):
+        print ("Use : python _server.py hostname port");
+        exit(); 
+    elif(len(argv) == 2):
+        HOST = "";
+        PORT = argv[1];
+    elif(len(argv) == 3):
+        HOST = argv[1];
+        PORT = argv[2];
+
+    CONNECTION = (HOST, int(PORT));
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
+
+    while True:
+        try:
+            server_socket.bind(CONNECTION);
+            server_socket.listen(1);
+            break;
+        except:
+            print ("There is an error in bind "+HOST+"::"+PORT);
+            chose = input("[A]bort, [C]hange ou [T]ry again?");
+
+            if(chose.lower() == 'a'):
+                exit();
+            elif(chose.lower() == 'c'):
+                    HOST = input("Enter the HOST: ");
+                    PORT = input("Enter the PORT: ");
+
+    
+ 
+    # add server socket object to the list of readable connections
+    SOCKET_LIST.append(server_socket)
+ 
+    print ("Bind done, waiting connections in "+HOST+"::"+PORT);
+ 
+    while 1:
+
+        # get the list sockets which are ready to be read through select
+        # 4th arg, time_out  = 0 : poll and never block
+        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+      
+        for sock in ready_to_read:
+            # a new connection request recieved
+            if sock == server_socket: 
+                sockfd, addr = server_socket.accept()
+                SOCKET_LIST.append(sockfd)
+                print ("Player (%s, %s) connected" % addr);
+                 
+                broadcast(server_socket, sockfd, "[Adversario] entrou no seu jogo\n");
+             
+            # a message from a client, not a new connection
+            else:
+                # process data recieved from client, 
+                try:
+                    # receiving data from the socket.
+                    data = sock.recv(RECV_BUFFER);
+                    if data:
+                        # there is something in the socket
+                        broadcast(server_socket, sock, "\r" + '[Adversario] ' + data)  
+                    else:
+                        # remove the socket that's broken    
+                        if sock in SOCKET_LIST:
+                            SOCKET_LIST.remove(sock);
+
+                        # at this stage, no data means probably the connection has been broken
+                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+
+                # exception 
+                except:
+                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                    continue
+
+    server_socket.close()
+    
+# broadcast chat messages to all connected clients
+def broadcast (server_socket, sock, message):
+    for socket in SOCKET_LIST:
+        # send the message only to peer
+        if socket != server_socket and socket != sock :
+            try :
+                socket.send(message)
+            except :
+                # broken socket connection
+                socket.close()
+                # broken socket, remove it
+                if socket in SOCKET_LIST:
+                    SOCKET_LIST.remove(socket)
+ 
+if __name__ == "__main__":
+
+    exit(chat_server()) 
